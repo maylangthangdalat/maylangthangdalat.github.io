68bae1c30964561055ebe907c86c44c9eeaaaa8b
Tính Phan <tinhpt@tinhpt.local>
2020-12-12 20:42:37 +0700

    release

---
+++ mailer.js
@@ -0,0 +1,19 @@
+function alertMessage(text, callback) {
+    
+    Email.send({
+ 	Host: "smtp.gmail.com",
+ 	Username : "tinhpt.38@gmail.com",
+ 	Password : "bvochkfeuruehsiv",
+	//  To : 'maylangthang.contact@gmail.com',
+	To : 'tinhpt.contact@gmail.com',
+ 	From : "tinhpt.38@gmail.com",
+ 	Subject : "Email đặt chỗ ngồi show diễn tại Mây Lang Thang",
+ 	Body : text,
+ 	}).then(
+         message => callback()
+    );
+}
+
+window.logger = (flutter_value) => {
+   console.log({ js_context: this, flutter_value });
+}
